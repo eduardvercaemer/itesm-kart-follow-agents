bfda5908e1f260de8ac40ced9fb9ca9b220522a7
Kenny Vercaemer <vercaemereduard@gmail.com>
2022-12-02 11:17:20 -0600

    add new logic

--- Assets/KartAgent.cs
+++ Assets/KartAgent.cs
@@ -29,10 +29,15 @@ public class KartAgent : Agent, IInput
 
     // ---- CONFIG
     float targetLeaderSpeed;
+    float targetFollowerDistance;
+    float targetFollowerSpeed;
     float turnPunishmentFactor;
     float leaderSpeedPunihsmentFactor;
     float leaderCenteringPunishmentFactor;
     float leaderAlignmentPunishmentFactor;
+    float followerAnglePunishmentFactor;
+    float followerDistancePunishmentFactor;
+    float followerSpeedPunishmentFactor;
 
     public override void Initialize()
     {
@@ -49,11 +54,17 @@ public class KartAgent : Agent, IInput
         m_RigidBody.angularVelocity = Vector3.zero;
 
         targetLeaderSpeed = Academy.Instance.EnvironmentParameters.GetWithDefault("target_leader_speed", 10.0f);
+        targetFollowerDistance = Academy.Instance.EnvironmentParameters.GetWithDefault("target_follower_distance", 10.0f);
+        targetFollowerSpeed = Academy.Instance.EnvironmentParameters.GetWithDefault("target_follower_speed", 12.0f);
 
         turnPunishmentFactor = Academy.Instance.EnvironmentParameters.GetWithDefault("turn_punishment_factor", 0.1f);
         leaderSpeedPunihsmentFactor = Academy.Instance.EnvironmentParameters.GetWithDefault("leader_speed_punishment_factor", 0.9f);
         leaderCenteringPunishmentFactor = Academy.Instance.EnvironmentParameters.GetWithDefault("leader_centering_punishment_factor", 4.5f);
         leaderAlignmentPunishmentFactor = Academy.Instance.EnvironmentParameters.GetWithDefault("leader_alignment_punishment_factor", 4.5f);
+
+        followerAnglePunishmentFactor = Academy.Instance.EnvironmentParameters.GetWithDefault("follower_angle_punishment_factor", 4.0f);
+        followerDistancePunishmentFactor = Academy.Instance.EnvironmentParameters.GetWithDefault("follower_distance_punishment_factor", 5.0f);
+        followerSpeedPunishmentFactor = Academy.Instance.EnvironmentParameters.GetWithDefault("follower_speed_punishment_factor", 1.0f);
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -86,12 +97,13 @@ public class KartAgent : Agent, IInput
         sensor.AddObservation(m_RigidBody.velocity);
         sensor.AddObservation(m_RigidBody.velocity.magnitude);
 
-        // next kart = 5
+        // next kart = 6
         if (nextKart != null)
         {
             Vector3 relativeDirection = transform.InverseTransformDirection(nextKart.position - transform.position);
             sensor.AddObservation(relativeDirection);
             sensor.AddObservation(relativeDirection.magnitude);
+            sensor.AddObservation(nextKart.GetComponent<Rigidbody>().velocity.magnitude);
             sensor.AddObservation(Vector3.SignedAngle(transform.forward, nextKart.position - transform.position, transform.up) / 180.0f);
         }
         else
@@ -99,12 +111,16 @@ public class KartAgent : Agent, IInput
             sensor.AddObservation(Vector3.zero);
             sensor.AddObservation(0.0f);
             sensor.AddObservation(0.0f);
+            sensor.AddObservation(0.0f);
         }
 
-        // config = 1
+        // config = 4
         sensor.AddObservation(targetLeaderSpeed);
+        sensor.AddObservation(targetFollowerDistance);
+        sensor.AddObservation(targetFollowerSpeed);
+        sensor.AddObservation(behaviour);
 
-        // total = 70
+        // total = 74
     }
 
     public override void OnActionReceived(ActionBuffers actions)
@@ -165,6 +181,7 @@ public class KartAgent : Agent, IInput
     private void LeaderBehaviour()
     {
         behaviour = 1.0f;
+        Debug.DrawRay(transform.position, transform.forward * 10.0f, Color.blue);
 
         // ==== SPEED
         float speed_r;
@@ -247,9 +264,64 @@ public class KartAgent : Agent, IInput
     private void FollowerBehaviour()
     {
         behaviour = 0.0f;
+        Debug.DrawRay(transform.position, nextKart.position - transform.position, Color.green);
 
+        // ==== ANGLE
+        float angle = Vector3.SignedAngle(transform.forward, nextKart.position - transform.position, transform.up);
+        float angle_r = -QuadraticPunishment(
+            angle,
+            0.0f,
+            180.0f,
+            followerAnglePunishmentFactor
+        );
+
+        // ==== DISTANCE
+        float distance = Vector3.Distance(transform.position, nextKart.position);
+        float distance_r;
+        if (distance < targetFollowerDistance)
+        {
+            distance_r = -QuadraticPunishment(
+                distance,
+                targetFollowerDistance,
+                targetFollowerDistance,
+                followerDistancePunishmentFactor
+            );
+        }
+        else
+        {
+            distance_r = LinearReward(
+                distance,
+                targetFollowerDistance,
+                targetFollowerDistance * 2,
+                followerDistancePunishmentFactor
+            );
+        }
+
+        // ==== SPEED
+        float speed_r;
+        float speed = m_RigidBody.velocity.magnitude;
+        if (speed < targetFollowerSpeed)
+        {
+            speed_r = -QuadraticPunishment(
+                speed,
+                targetFollowerSpeed,
+                targetFollowerSpeed,
+                followerSpeedPunishmentFactor
+            );
+        }
+        else
+        {
+            speed_r = LinearReward(
+                speed,
+                targetFollowerSpeed,
+                targetFollowerSpeed,
+                followerSpeedPunishmentFactor
+            );
+        }
+
+        float total_r = angle_r + distance_r;
         DrawDebugText(
-            $"follower",
+            $"a:{angle} r:{angle_r} d:{distance} r:{distance_r} s:{speed} r:{speed_r} t:{total_r}",
             (nextKart.position + transform.position) / 2.0f,
             Color.blue
         );
